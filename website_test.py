2890e4cdf3d4a5f0f7f7587d177c041f8525f919
blokxblok <145865658+blokxblok@users.noreply.github.com>
2023-09-27 20:32:53 -0700

    Add files via upload
    Added to file

--- website_test.py
+++ website_test.py
@@ -8,7 +8,7 @@ topic = st.selectbox("Please select one topic to generate ideas on.",
 if(st.button("Submit")):
     st.subheader("Some ideas to get you started")
     if topic == str("Music"):
-        if st.button("Focus on rhythm first"):
-            st.text("A good melody relies of a good rhythm, as does a good song. Starting with rhythm is generally a lot easier than composing the actual contour of the melody.")
+        first = st.write("Focus on rhythm first")
+        second = st.write("Get visually inspired")
     st.subheader("Find some more ideas")
     st.write("go touch grass")
